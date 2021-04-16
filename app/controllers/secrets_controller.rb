c57080f6f3835b957c4b2a5ae87f3790aca213d1
Jbeltrez <beltrezjohn@gmail.com>
2021-04-16 14:27:45 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,9 @@
+class SecretsController < ApplicationController
+    before_action :require_logged_in 
+    def show 
+        require_logged_in
+
+    end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
+  get '/login', {:action=>"new", :controller=>"sessions"}
+
+  post 'login', {:action=>"create", :controller=>"sessions"}
+  get 'secrets', {:action=>"show", :controller=>"secrets"}
+  post 'logout', {:action=>"destroy", :controller=>"sessions"}
+
 end
